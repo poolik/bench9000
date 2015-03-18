f46ae5c038301840a0c20ad7faa468a6ca228f9d
Chris Seaton <chris@chrisseaton.com>
2015-03-18 09:13:08 -0600

    Fix for handling crashing subprocesses.

--- lib/bench/implementation.rb
+++ lib/bench/implementation.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved. This
+# Copyright (c) 2014, 2015 Oracle and/or its affiliates. All rights reserved. This
 # code is released under a tri EPL/GPL/LGPL license. You can use it,
 # redistribute it and/or modify it under the terms of the:
 # 
@@ -39,7 +39,7 @@ module Bench
           line = subprocess.gets
           line.strip! unless line.nil?
 
-          if line.start_with? '[truffle]'
+          if !line.nil? && line.start_with?('[truffle]')
             STDERR.puts line
             next
           end
