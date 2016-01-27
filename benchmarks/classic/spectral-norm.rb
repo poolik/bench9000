fdfcf71dcfc5e626425e827e37ce9adbe1006082
Benoit Daloze <eregontp@gmail.com>
2016-01-27 11:04:11 +0100

    Fix indentation in spectral-norm

--- benchmarks/classic/spectral-norm.rb
+++ benchmarks/classic/spectral-norm.rb
@@ -35,29 +35,29 @@
 # http://benchmarksgame.alioth.debian.org/u64q/program.php?test=spectralnorm&lang=yarv&id=1
 
 def eval_A(i,j)
-  return 1.0/((i+j)*(i+j+1)/2+i+1)
+  return 1.0 / ((i+j)*(i+j+1)/2+i+1)
 end
 
 def eval_A_times_u(u)
-        v, i = nil, nil
+  v, i = nil, nil
   (0..u.length-1).collect { |i|
-                v = 0
+    v = 0
     for j in 0..u.length-1
-      v += eval_A(i,j)*u[j]
-                end
-                v
-        }
+      v += eval_A(i,j) * u[j]
+    end
+    v
+  }
 end
 
 def eval_At_times_u(u)
   v, i = nil, nil
-  (0..u.length-1).collect{|i|
-                v = 0
+  (0..u.length-1).collect { |i|
+    v = 0
     for j in 0..u.length-1
-      v += eval_A(j,i)*u[j]
-                end
-                v
-        }
+      v += eval_A(j,i) * u[j]
+    end
+    v
+  }
 end
 
 def eval_AtA_times_u(u)
@@ -65,19 +65,19 @@ def eval_AtA_times_u(u)
 end
 
 def spectral_norm(n)
-  u=[1]*n
+  u = [1] * n
   for i in 1..10
-          v=eval_AtA_times_u(u)
-          u=eval_AtA_times_u(v)
+    v = eval_AtA_times_u(u)
+    u = eval_AtA_times_u(v)
   end
-  vBv=0
-  vv=0
+  vBv = 0
+  vv  = 0
   for i in 0..n-1
-          vBv += u[i]*v[i]
-          vv += v[i]*v[i]
+    vBv += u[i] * v[i]
+    vv  += v[i] * v[i]
   end
 
-  Math.sqrt(vBv/vv)
+  Math.sqrt(vBv / vv)
 end
 
 def harness_input
